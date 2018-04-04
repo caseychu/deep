d30553ad9f9ceebd35faa541451ab5fbd1fbe7c3
Casey Chu <caseychu@stanford.edu>
2018-04-04 15:25:47 -0700

    evaluate UPDATE_OPS when training

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ from deep import op
 
 @op
 def minimize(optimizer, loss, var_list=None, global_step=None):
-    #with tf.control_dependencies(update_ops):
-    minimize_op = optimizer.minimize(loss, var_list=var_list, global_step=global_step)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        minimize_op = optimizer.minimize(loss, var_list=var_list, global_step=global_step)
     
     return minimize_op
